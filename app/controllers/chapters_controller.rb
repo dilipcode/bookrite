0b5499862a9c17d12dbc2ab411d1eb9979b040c3
dilipcode <drsar8@gmail.com>
2016-05-02 19:57:13 +0530

    Added ability to delete chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,6 +1,6 @@
 class ChaptersController < ApplicationController
   before_action :fetch_book
-  before_action :fetch_chapter, only: [:edit,:update]
+  before_action :fetch_chapter, only: [:edit,:update,:destroy]
   def create
     @chapter = @book.chapters.new(chapter_params)
     @save_success = !!@chapter.save
@@ -13,6 +13,11 @@ class ChaptersController < ApplicationController
   def update
     @chapter.update_attributes(chapter_params)
     @chapter.reload unless @chapter.valid?
+  end
+
+  def destroy
+    @chapter.destroy
+
   end
 
 
