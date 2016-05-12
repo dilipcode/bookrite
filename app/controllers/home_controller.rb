7ef2c82391730d2bb5e48aad5c09788504e27a5f
dilipcode <drsar8@gmail.com>
2016-05-12 10:51:31 +0530

    Authentication required for all pages except Home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:land, :translate]
   def land
   end
+
   def translate
     I18n.locale = params[:locale].to_sym
     redirect_to :back
